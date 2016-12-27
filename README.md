76c5a8ae1877292e53ed2710c455c974c5e17698
Jacob Allen Utley <jacob@halfinjest.com>
2016-12-27 01:18:04 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Revolution
+
+Programs for the Nintendo Wii (Revolution) through the Homebrew Channel.
+
+These programs can be executed through the Homebrew Channel. Homebrew must be installed on the Wii, and an application SD card must also be created. A guide to installing Homebrew and creating an application SD card can be found on their [web site](http://wiibrew.org/wiki/Homebrew_setup).
+
+## Usage
+
+Navigate to the directory of the desired program, then compile it:
+
+`make`
+
+A directory for this program is made under the `apps` directory on the SD card.
+
+The `.dol` file is copied to this new directory as `boot.dol`.
+
+Optionally, the `icon.png` and `meta.xml` files can also be copied to this directory. This information shows up on the Homebrew menu, but it is not necessary when executing an application.
---
+++ roll-Wii/source/main.c
@@ -0,0 +1,46 @@
+#include <gccore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiiuse/wpad.h>
+#include "roll.h"
+
+#define FALSE 0
+#define LENGTH 4
+#define TRUE 1
+
+void setup()
+{
+	static void *xfb = NULL;
+	static GXRModeObj *rmode = NULL;
+	VIDEO_Init();
+	WPAD_Init();
+	rmode = VIDEO_GetPreferredMode(NULL);
+	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+	console_init(xfb, 20, 20, rmode -> fbWidth, rmode -> xfbHeight,
+		rmode -> fbWidth * VI_DISPLAY_PIX_SZ);
+	VIDEO_Configure(rmode);
+	VIDEO_SetNextFramebuffer(xfb);
+	VIDEO_SetBlack(FALSE);
+	VIDEO_Flush();
+	VIDEO_WaitVSync();
+	if(rmode -> viTVMode & VI_NON_INTERLACE) VIDEO_WaitVSync();
+}
+
+int main(int argc, char **argv)
+{
+	int i, *dice = malloc(sizeof(int) * LENGTH);
+	srand(time(NULL));
+	setup();
+	printf("\x1b[2;0H");
+	for (i = 0; i < LENGTH; i++) dice[i] = rand() % 6;
+	roll(LENGTH, dice);
+	printf("Press the HOME button to return.");
+	while(TRUE)
+	{
+		WPAD_ScanPads();
+		u32 button = WPAD_ButtonsDown(0);
+		if (button & WPAD_BUTTON_HOME) exit(0);
+		VIDEO_WaitVSync();
+	}
+	return 0;
+}
