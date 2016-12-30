d5e5de5b0693c0d9c2bcbae6e35bedacc6745c1d
Jacob Allen Utley <jacob@halfinjest.com>
2016-12-30 17:42:59 -0600

    Syntax changes.

--- roll-Wii/source/main.c
+++ roll-Wii/source/main.c
@@ -5,25 +5,26 @@
 #include "roll.h"
 
 #define FALSE 0
-#define LENGTH 4
 #define TRUE 1
+
+static void *xfb = NULL;
+
+static GXRModeObj *rmode = NULL;
 
 void setup()
 {
-	static void *xfb = NULL;
-	static GXRModeObj *rmode = NULL;
 	VIDEO_Init();
 	WPAD_Init();
 	rmode = VIDEO_GetPreferredMode(NULL);
 	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
-	console_init(xfb, 20, 20, rmode -> fbWidth, rmode -> xfbHeight,
-		rmode -> fbWidth * VI_DISPLAY_PIX_SZ);
+	console_init(xfb, 20, 20, rmode->fbWidth, rmode->xfbHeight,
+		rmode->fbWidth * VI_DISPLAY_PIX_SZ);
 	VIDEO_Configure(rmode);
 	VIDEO_SetNextFramebuffer(xfb);
 	VIDEO_SetBlack(FALSE);
 	VIDEO_Flush();
 	VIDEO_WaitVSync();
-	if(rmode -> viTVMode & VI_NON_INTERLACE) VIDEO_WaitVSync();
+	if(rmode->viTVMode & VI_NON_INTERLACE) VIDEO_WaitVSync();
 }
 
 int main()
@@ -35,7 +36,7 @@ int main()
 	{
 		WPAD_ScanPads();
 		u32 button = WPAD_ButtonsDown(0);
-		if (button & WPAD_BUTTON_A) roll(LENGTH);
+		if (button & WPAD_BUTTON_A) roll(4);
 		else if (button & WPAD_BUTTON_HOME) exit(0);
 		VIDEO_WaitVSync();
 	}
